201f269eb6bf5f017548e60d983ef0f4d1cb2742
al-uminium <79945949+al-uminium@users.noreply.github.com>
2022-05-11 01:36:08 +0800

    skeleton html, css and js added

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+
+//initialize library in array
+
+let myLibrary = [];
+
+//Book constructor
+function Book (title, author, page, haveRead) {
+    this.title = title;
+    this.author = author;
+    this.page = page;
+    this.readStatus = haveRead ? "have read" : "have not read";
+    this.info = function() {
+        console.log(`${this.title} by ${this.author}, ${page} pages, ${this.readStatus}`)
+    }
+}
+
+function addBookToLibrary() {
+    //to fill it up later
+}
