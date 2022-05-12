ed04dfa03e37a2d791ca320067c8ec2588794a15
al-uminium <79945949+al-uminium@users.noreply.github.com>
2022-05-12 23:53:12 +0800

    added modal form css & basic js queries and helper functions

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 //query selectors
-const addBookBtn = document.querySelector("#add-book")
+const addBookBtn = document.querySelector("#add-book");
+const modalAddBookBtn = document.querySelector("#modal-add-book")
+const closeModalBtn = document.querySelector(".close-btn");
+const modalForm = document.querySelector("#modal-form");
+
+const bookTitleInput = document.querySelector("#book-title");
+const bookAuthorInput = document.querySelector("#book-author");
+const bookPageInput = document.querySelector("#book-page");
+const bookReadInput = document.querySelector("#book-read")
+
 
 
 //initialize library in array
@@ -17,6 +26,31 @@ function Book (title, author, page, haveRead) {
     }
 }
 
-function addBookToLibrary() {
-    //to fill it up later
+function addBookToLibrary(title, author, page, haveRead) {
+    const newBook = Book(title, author, page, haveRead)
+    myLibrary.push(newBook)
 }
+
+const getModalInputValues = () => {
+    const bookTitle = bookTitleInput.value;
+    const bookAuthor = bookAuthorInput.value;
+    const bookPage = bookPageInput.value;
+    const bookReadStatus = bookAuthorInput.checked;
+
+    return [
+        bookTitle, 
+        bookAuthor,
+        bookPage,
+        bookReadStatus
+    ]
+}
+
+//event listeners
+closeModalBtn.addEventListener("click", () => {
+    modalForm.classList.toggle("close")
+})
+
+addBookBtn.addEventListener("click", () => {
+    modalForm.classList.toggle("close")
+})
+
